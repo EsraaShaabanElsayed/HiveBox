943e6c5c64e655fec2430281014a2c2d8335988d
esraa <shaabanesraa02@gmail.com>
2025-01-11 23:56:12 +0200

    fix:updated temp test

--- temperture_api_test.py
+++ temperture_api_test.py
@@ -1,6 +1,6 @@
-""" This module contains tests for the Flask app's routes and endpoints. It includes a test for the temperature endpoint."""
+""" This module contains tests for the Flask app's routes and endpoints.
+It includes a test for the temperature endpoint."""
 
-import os
 import unittest
 from datetime import datetime, timedelta, timezone
 from unittest.mock import patch
@@ -11,12 +11,6 @@ from main import app
 class TestTemperatureRoute(unittest.TestCase):
     """Test case for the /temperature endpoint"""
 
-    @patch.dict(
-        os.environ,
-        {
-            "SENSOR_IDS": '["5eba5fbad46fb8001b799786", "5e60cf5557703e001bdae7f8", "5eb99cacd46fb8001b2ce04c"]'
-        },
-    )
     def test_temperature_route(self):
         """Test the temperature endpoint"""
         mock_data = {
